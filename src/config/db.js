876028b27e9b92f8ec9764eb742f00515727d0ec
KRISHH CHAUHAN <95200141+djkrishnachauhan@users.noreply.github.com>
2025-12-28 00:45:06 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 module.exports = async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI, { dbName: "TALLY" });
+  await mongoose.connect(process.env.MONGO_URI, { dbName: "TALLY_RT" });
   console.log("MongoDB Connected");
 };
